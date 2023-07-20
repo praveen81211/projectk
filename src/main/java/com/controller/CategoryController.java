d7cbc382f0d2d6067d165a7a692e6276d691a078
praveen81211 <139943701+praveen81211@users.noreply.github.com>
2023-07-20 12:44:41 +0530

    Update CategoryController.java

--- src/main/java/com/controller/CategoryController.java
+++ src/main/java/com/controller/CategoryController.java
@@ -22,6 +22,6 @@ public class CategoryController {
 	@Operation(summary = "list all the available categories")
 	public List<Category> listofcat() {
 		return categoryRepo.findAll();
-     
+
 	}
 }
